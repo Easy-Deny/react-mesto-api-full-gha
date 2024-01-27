b28edb5e59726727a5681453915fffcf55791013
Denis Knor <knorred@mail.ru>
2024-01-27 18:15:23 +0300

    feat: move backend to backend folder

---
+++ backend/app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const { errors } = require('celebrate');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+const errorHandler = require('./errors/error-handler');
+
+const NotFoundError = require('./errors/not-found-error');
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+})
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch(() => {
+    console.log('Not connected to MongoDB');
+  });
+const app = express();
+const PORT = 3000;
+const userRouter = require('./routes/users');
+const cardRouter = require('./routes/cards');
+
+app.use(express.json());
+app.use(requestLogger);
+app.use(userRouter);
+app.use(cardRouter);
+app.use(errorLogger);
+app.use(errors());
+app.all('*', (req, res, next) => {
+  next(new NotFoundError('404! Page not found'));
+});
+app.use(errorHandler);
+app.listen(PORT, () => {
+  console.log(`port ${PORT}`);
+});
---
+++ backend/routes/users.js
@@ -0,0 +1,48 @@
+const { celebrate, Joi } = require('celebrate');
+const Router = require('express').Router();
+const {
+  createUser,
+  getUsers,
+  getUserById,
+  updateUserById,
+  updateUser,
+  updateUserAvatarById,
+  login,
+} = require('../controllers/users');
+const auth = require('../middlewares/auth');
+
+Router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+Router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+    avatar: Joi.string().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), createUser);
+Router.use(auth);
+Router.get('/users/me', updateUser);
+Router.get('/users/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().length(24).hex().required(),
+  }),
+}), getUserById);
+Router.get('/users', getUsers);
+Router.patch('/users/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    about: Joi.string().required().min(2).max(30),
+  }),
+}), updateUserById);
+Router.patch('/users/me/avatar', celebrate({
+  body: Joi.object().keys({
+    avatar: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
+  }),
+}), updateUserAvatarById);
+module.exports = Router;
